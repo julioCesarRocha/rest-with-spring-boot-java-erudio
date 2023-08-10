0e9b7addb8261f6c721f289bf6a5fca1ebd7f49c
Julio Cesar <julio.junior1@estudante.ifb.edu.br>
2023-08-10 18:58:23 -0300

    feat: Initial Commit

---
+++ FirstStepsInJavaWithSpringBoot/rest-with-spring-boot-java-erudio/rest-with-spring-boot-java-erudio/src/main/java/br/com/restspringboot/restwithspringbootjavaerudio/RestWithSpringBootJavaErudioApplication.java
@@ -0,0 +1,13 @@
+package br.com.restspringboot.restwithspringbootjavaerudio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestWithSpringBootJavaErudioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestWithSpringBootJavaErudioApplication.class, args);
+	}
+
+}
