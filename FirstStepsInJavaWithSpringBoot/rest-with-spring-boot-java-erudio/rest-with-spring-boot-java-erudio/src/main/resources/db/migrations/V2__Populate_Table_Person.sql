e846ca0682039231968d162f93b7fffdbf6c1475
Julio Cesar <julio.junior1@estudante.ifb.edu.br>
2023-09-13 16:58:54 -0300

    feat:Implementando suporte á migration com Flyway.

---
+++ FirstStepsInJavaWithSpringBoot/rest-with-spring-boot-java-erudio/rest-with-spring-boot-java-erudio/src/main/resources/db/migrations/V2__Populate_Table_Person.sql
@@ -0,0 +1,8 @@
+INSERT INTO rest_with_spring_boot.person VALUES
+    (1,'Brasilia-DF','Julio','Male','Cesar'),
+    (2,'Brasilia-DF','Maria','Male','Jose'),
+    (3,'Rio de Janeiro','Jorge','Male','Amado'),
+    (4,'Rio de Janeiro','Maria','Female','Bethania'),
+    (6,'Sao Paulo','Joao','Male','Lipsulino'),
+    (8,'Brasilia-DF','Agripina','Female','Rocha');
+
